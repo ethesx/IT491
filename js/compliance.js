1dcd9310fb4449681bcab13e5f7d9f40e03a04f3
ethesx <ethesx@gmail.com>
2016-11-14 21:49:28 -0500

    Simplified, corrected dynamic checkbox button additions

--- js/compliance.js
+++ js/compliance.js
@@ -1,35 +1,20 @@
 var controlGroupName = "#complianceItems";
 var cases = data[dataIndexSelected].Case;
-var group = $( controlGroupName );
-/*
 
-$(document).bind('pagecreate', "#compliance", function(e, data){
-    var conditions;
+$(document).on('pagecreate', "#compliance", function(e, data){
+    var group = $( controlGroupName );
     var counter = 0;
+
     $.each(cases,function(index, value){
 
-
+        console.log(cases[index].CaseNumber);
         $.each(cases[index].MonitoringCondition,function(index, value) {
             counter++;
-               var widgetType = $( "[name='radio-widget']:checked" ).attr( "id" ),
-                group,
-                $el,
-                action = function() {
-                    var action = $( "[name='radio-action']:checked" ).attr( "id" );
-                    if ( $( $el[1] ).is( "select" ) && action === "hide" ) {
-                        $el = $($el[1]).parents(".ui-select");
-                    }
-                    $el[ action ]();
-                    group.controlgroup( "refresh" );
-                };
-            $el = $( "<label for='widget" + counter + "'>"+ value.Condition + "</label><input type='checkbox' id='widget" + counter + "'></input>" );
-            $( $el[ 1 ] ).bind( "change", action );
-            $( controlGroupName ).controlgroup( "container" )[ "append" ]($el);
-            $( $el[ 1 ] ).checkboxradio();
+            group.controlgroup("container")
+                .append("<label for='widget" + counter + "'>"+ value.Condition + "</label><input type='checkbox' id='widget" + counter + "' />");
         });
-
     });
-    group.controlgroup("refresh");
+    group.enhanceWithin().controlgroup("refresh");
 
     //TODO how is the compliance item to be posted back as selected - by name, index, etc?
-});*/
+});
