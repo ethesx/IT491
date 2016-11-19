0be3959559b4e827ed3ab0b2646754bbc08ec269
ethesx <ethesx@gmail.com>
2016-11-19 14:37:44 -0500

    Updated edit functionality, styling

--- js/contact.js
+++ js/contact.js
@@ -1,59 +1,9 @@
 $(document).on('pagecreate', "#defContactInfo", function() {
-    $('#phoneHome1 #phoneHome2 #phoneMobile1 #phoneMobile2').parent().removeClass('ui-input-text ui-shadow-inset');
 
-//TODO Refactor
-    $('#phoneHome1Edit').click(function () {
-        $('#phoneHome1')
-            .removeClass('transparent')
-            .prop('disabled', false)
-            .parent()
-            .addClass('ui-input-text ui-shadow-inset')
-            .removeClass('ui-state-disabled');
-    });
-
-    $('#phoneHome2Edit').click(function () {
-        $('#phoneHome2')
-            .removeClass('transparent')
-            .prop('disabled', false)
-            .parent()
-            .addClass('ui-input-text ui-shadow-inset')
-            .removeClass('ui-state-disabled');
-    });
-
-    $('#phoneMobile1Edit').click(function () {
-        $('#phoneMobile1')
-            .removeClass('transparent')
-            .prop('disabled', false)
-            .parent()
-            .addClass('ui-input-text ui-shadow-inset')
-            .removeClass('ui-state-disabled');
-    });
-
-    $('#phoneMobile2Edit').click(function () {
-        $('#phoneMobile2')
-            .removeClass('transparent')
-            .prop('disabled', false)
-            .parent()
-            .addClass('ui-input-text ui-shadow-inset')
-            .removeClass('ui-state-disabled');
-    });
-
-    $('#email1Edit').click(function () {
-        $('#email1')
-            .removeClass('transparent')
-            .prop('disabled', false)
-            .parent()
-            .addClass('ui-input-text ui-shadow-inset')
-            .removeClass('ui-state-disabled');
-    });
-
-    $('#email2Edit').click(function () {
-        $('#email2')
-            .removeClass('transparent')
-            .prop('disabled', false)
-            .parent()
-            .addClass('ui-input-text ui-shadow-inset')
-            .removeClass('ui-state-disabled');
+    Utils.toggleInset();
+    $('#contactInfoEdit').click(function () {
+        Utils.toggleInputText();
+        Utils.toggleInset();
     });
 
     //Data population --------------------------------------------------------------------------------------
