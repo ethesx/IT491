6549862f6499fed17c29d8f9cd3f4d47e577c45d
ethesx <ethesx@gmail.com>
2016-11-21 17:09:11 -0500

    Corrected undefined at start of defendant name list, name case,  capitalization

--- js/name.js
+++ js/name.js
@@ -1,13 +1,13 @@
 $(document).on('pagecreate', "#defList", function(){
-    var output;
+    var output = "";
     var array = [];
     $.each(data, function (index, value) {
-        array.push(value.DemographicInfo.LastName + ", " + value.DemographicInfo.FirstName);
+        array.push(value.DemographicInfo.LastName.toLowerCase() + ", " + value.DemographicInfo.FirstName.toLowerCase());
     });
     array.sort();
 
     for(var i = 0; i < array.length; i++) {
-        output += '<li><a href="#">' + array[i] + '</a></li>';
+        output += '<li><a href="#" class="defendantName">' + array[i] + '</a></li>';
     }
     $('#myList').html(output).listview("refresh");
 });
