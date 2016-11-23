fff3225b228a2bb4b48b4cd3cf0c9f475f3aa31a
dlm24 <dlm24@njit.edu>
2016-11-23 07:15:51 -0500

    Added slider event for calendar. This script also contains the post back function.

--- js/complete.js
+++ js/complete.js
@@ -1,5 +1,12 @@
 /**
  * Created by Dominic on 11/22/2016.
+ * This is my script that runs the POST upon the complete interview button is clicked (#proceed). I have to add the json
+ * object that we will be returning.
+ *
+ * The second function handles the slider event for the question " Do you need to schedule an appointment for the
+ * defendant?"  Currently, if slid in any direction it will call the function which will create a popup form for the
+ * field officer to schedule the next appoointment. It should provide a date picker, time picker, and location of
+ * interview. This will be saved to the object to be posted back to the database.
  */
 
 $(function () {
@@ -26,4 +33,9 @@ $(function () {
         }) ;
 
     });
+
+    $( "#flip-1" ).on( 'slidestop', function( event ) {
+        alert('Popup Calendar<br>with time and location parameters.<br>');
+    });
+
 });
