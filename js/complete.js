02c2b505df8365487fc7fcf277dc9ed82a6ff265
dlm24 <dlm24@njit.edu>
2016-11-23 07:24:14 -0500

    Added comments

--- js/complete.js
+++ js/complete.js
@@ -1,12 +1,14 @@
 /**
  * Created by Dominic on 11/22/2016.
  * This is my script that runs the POST upon the complete interview button is clicked (#proceed). I have to add the json
- * object that we will be returning.
+ * object that we will be returning. I am using the jquery shorthand for the document ready.
  *
  * The second function handles the slider event for the question " Do you need to schedule an appointment for the
  * defendant?"  Currently, if slid in any direction it will call the function which will create a popup form for the
  * field officer to schedule the next appoointment. It should provide a date picker, time picker, and location of
  * interview. This will be saved to the object to be posted back to the database.
+ *
+ * Once the POST is working correctly, The confirmation page details will be displayed as a result/ PDF created.
  */
 
 $(function () {
@@ -35,7 +37,7 @@ $(function () {
     });
 
     $( "#flip-1" ).on( 'slidestop', function( event ) {
-        alert('Popup Calendar<br>with time and location parameters.<br>');
+        alert('Popup Calendar with time and location parameters.');
     });
 
 });
