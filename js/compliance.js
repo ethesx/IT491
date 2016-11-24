a26035bf5e2b133bbb436d522b04e2a4afb11a5a
ethesx <ethesx@gmail.com>
2016-11-23 23:26:15 -0500

    Added response object updates for compliance

--- js/compliance.js
+++ js/compliance.js
@@ -4,6 +4,9 @@ $(document).on('pagecreate', "#compliance", function(){
     var cases = data[dataIndexSelected].Case;
     var counter = 0;
 
+    /*TODO SERVICE DATA ISSUE - initial data indicates both condition.IsCompliant && condition.IsNonCompliant are false
+    We must know which one holds the correct value to read and initially set the conditions
+    */
     $.each(cases,function(index, value){
         var gn = "complianceItems" + index;
         $("<div/>", {
@@ -17,11 +20,27 @@ $(document).on('pagecreate', "#compliance", function(){
         $.each(cases[index].MonitoringCondition,function(index, value) {
             counter++;
             group.controlgroup("container")
-                .append("<label for='widget" + counter + "'>"+ value.Condition + "</label><input type='checkbox' id='widget" + counter + "' />");
+                .append("<label for='complianceItem" + counter + "'>"+ value.Condition + "</label><input type='checkbox' id='complianceItem" + counter + "' />");
         });
         group.enhanceWithin().controlgroup("refresh");
     });
 
+    //Attach change event handler to each monitoring item checkbox - updates response object's respective Case[?].MonitoringCondition[?] properties
+    $(controlGroupContainer + " input[type=checkbox]").change(function(event){
+        var parentSelector = controlGroupContainer + " .ui-controlgroup";
+        var caseIndex = $(parentSelector).index($(this).closest(parentSelector));
+        var conIndex = $("#" + $(this).closest(parentSelector).attr("id") + " input[type=checkbox]").index(this);
+        var condition = cases[caseIndex].MonitoringCondition[conIndex];
 
-    //TODO how is the compliance item to be posted back as selected - by name, index, etc?
+        console.debug("Case index : " + caseIndex);
+        console.debug("Condition index : " + conIndex);
+        console.debug("IsCompliant BEFORE : " + condition.IsCompliant);
+        console.debug("IsNonCompliant BEFORE : " + condition.IsNonCompliant);
+
+        condition.IsCompliant = !$(event.target).prop("checked");
+        condition.IsNonCompliant = $(event.target).prop("checked");
+
+        console.debug("IsCompliant AFTER : " + condition.IsCompliant);
+        console.debug("IsNonCompliant AFTER : " + condition.IsNonCompliant);
+    });
 });
