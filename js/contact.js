64fdf8b348714c0de8e2cf73af8e272a3f00c87b
ethesx <ethesx@gmail.com>
2016-11-25 12:40:26 -0500

    Updated phone formating, validation

--- js/contact.js
+++ js/contact.js
@@ -9,10 +9,17 @@ $(document).on('pagecreate', "#defContactInfo", function() {
 
     var elementArrayToSet = ['#primaryHome', '#supplementalContactPhone', '#primaryMobile', '#secondaryMobile', '#email', '#supplementalContactEmail'];
     var responseCd = response.ContactDetails;
+
+    //Format phone numbers on change
+    $('#primaryHome, #supplementalContactPhone, #primaryMobile, #secondaryMobile').change(function(){
+        $(this).val($(this).val().substr(0,10).replace(/\D/g, '').replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3'));
+    });
+
     /*pass in element array to set event handler on,
      response object's object which properties for update are contained within,
      and the list of properties to update*/
     $(elementArrayToSet.join(", ")).change({obj : responseCd, propsArray : elementArrayToSet}, Utils.setResponseValue);
+
 
     //Data population --------------------------------------------------------------------------------------
 
